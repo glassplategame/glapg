9d12ccd5a50b4d35f0c254c24301ebdfda90361d
Wade Cline <wadecline@hotmail.com>
2014-01-15 08:45:42 -0800

    Add database models.

--- glapg/models.py
+++ glapg/models.py
@@ -1,3 +1,59 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import relationship
 from glapg.database import Model
 
+branches = Table('branch', Model.metadata,
+    Column('cube_id', Integer, ForeignKey('cube.id')),
+    Column('connection_id', Integer, ForeignKey('connection.id'))
+)
+
+class Board(Model):
+    __tablename__ = 'board'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(63))
+
+# Represents one if the player's moves.
+class Cube(Model):
+    __tablename__ = 'cube'
+
+    id = Column(Integer, primary_key=True)
+    board_id = Column(Integer, ForeignKey('board.id'))
+    board = relationship('Board')
+    # The sequential order in which this cube was placed.
+    number = Column(Integer)
+    state = Column(Integer)
+    created = Column(DateTime)
+
+# Represents one of the 'plates' in the Glass Plate Game.
+class Plate(Model):
+    __tablename__ = 'plate'
+
+    id = Column(Integer, primary_key=True)
+    board_id = Column(Integer, ForeignKey('board.id'))
+    board = relationship('Board')
+    # The plate's textual representation.
+    name = Column(String(63))
+    # The plate's visual representation.
+    image_name = Column(String(63))
+
+class Connection(Model):
+    __tablename__ = 'connection'
+
+    id = Column(Integer, primary_key=True)
+    board_id = Column(Integer, ForeignKey('board.id'))
+    board = relationship('Board')
+    connection_next_id = Column(Integer, ForeignKey('connection.id'))
+    connection_next = relationship('Connection')
+    # Player's description for the connection.
+    text = Column(String(1023))
+    cubes_prev = relationship('Cube', secondary=branches)
+    cube_next = Column(Integer, ForeignKey('cube.id'))
+
+'''class Train(Model):
+    __tablename__ = 'train'
+
+    id = Column(Integer, primary_key=True)
+    color = Column(Integer)
+    cube_initial = Column(Integer, ForeignKey('cube.id'))
+    connections = relationship('cube', secondary='''
